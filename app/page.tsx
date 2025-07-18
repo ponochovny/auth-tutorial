75e6c235241e31123a5e295577c20e9b8878ca47
ponochovny <ponochovny@gmail.com>
2025-07-18 03:05:48 +0300

    feat: UI

--- app/page.tsx
+++ app/page.tsx
@@ -11,23 +11,21 @@ const font = Poppins({
 
 export default function Home() {
 	return (
-		<main className='flex h-full flex-col items-center justify-center bg-radial-[at_50%_75%] from-sky-400 to-blue-800'>
-			<div className='space-y-6'>
-				<h1
-					className={cn(
-						'text-6xl font-semibold text-white drop-shadow-md',
-						font.className
-					)}
-				>
-					🔐Auth
-				</h1>
-				<p className='text-white text-lg'>A simple authentication service</p>
-				<LoginButton>
-					<Button variant='secondary' size='lg'>
-						Sign In
-					</Button>
-				</LoginButton>
-			</div>
+		<main className='flex h-full flex-col items-center justify-center bg-radial-[at_50%_75%] from-sky-400 to-blue-800 space-y-6'>
+			<h1
+				className={cn(
+					'text-6xl font-semibold text-white drop-shadow-md',
+					font.className
+				)}
+			>
+				🔐Auth
+			</h1>
+			<p className='text-white text-lg'>A simple authentication service</p>
+			<LoginButton>
+				<Button variant='secondary' size='lg'>
+					Sign In
+				</Button>
+			</LoginButton>
 		</main>
 	)
 }
