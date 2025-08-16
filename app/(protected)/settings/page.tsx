78cbc56736ecb16e88c9a3d16672dbd6642cbc7a
ponochovny <ponochovny@gmail.com>
2025-08-16 20:07:44 +0300

    feat: settings page, OAuth field, card width
    Added isOAuth field to the user, added Settings page to update user info, Card cmp now with full width by default

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,26 +1,94 @@
 'use client'
 
-import { logout } from '@/actions/logout'
-// import { useCurrentUser } from '@/hooks/use-current-user'
-// import { signOut, useSession } from 'next-auth/react'
+import * as z from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useSession } from 'next-auth/react'
+import { useTransition, useState } from 'react'
+
+import { SettingsSchema } from '@/schemas'
+import { settings } from '@/actions/settings'
+import { useCurrentUser } from '@/hooks/use-current-user'
+import { Button } from '@/components/ui/button'
+import { Card, CardHeader, CardContent } from '@/components/ui/card'
+import {
+	Form,
+	FormField,
+	FormControl,
+	FormItem,
+	FormLabel,
+	FormDescription,
+	FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import FormError from '@/components/form-error'
+import FormSuccess from '@/components/form-success'
 
 const SettingsPage = () => {
-	// const session = useSession()
-	// const user = useCurrentUser()
+	const user = useCurrentUser()
 
-	const onClick = () => {
-		// signOut()
-		logout()
+	const [error, setError] = useState<string | undefined>()
+	const [success, setSuccess] = useState<string | undefined>()
+	const { update } = useSession()
+	const [isPending, startTransition] = useTransition()
+
+	const form = useForm<z.infer<typeof SettingsSchema>>({
+		resolver: zodResolver(SettingsSchema),
+		defaultValues: {
+			name: user?.name || undefined,
+		},
+	})
+
+	const onSubmit = (values: z.infer<typeof SettingsSchema>) => {
+		startTransition(() => {
+			settings(values)
+				.then((data) => {
+					if (data.error) {
+						setError(data.error)
+					} else {
+						setSuccess(data.success)
+						update()
+					}
+				})
+				.catch(() => setError('Something went wrong'))
+		})
 	}
 
 	return (
-		<div className='flex flex-col gap-y-4 p-10 bg-white rounded-3xl'>
-			{/* <pre>{JSON.stringify(session)}</pre> */}
-			{/* <pre>{JSON.stringify(user)}</pre> */}
-			<button type='button' onClick={onClick}>
-				Sign out
-			</button>
-		</div>
+		<Card className='max-w-[600px]'>
+			<CardHeader>
+				<p className='text-2xl font-semibold text-center'>⚙️ Settings</p>
+			</CardHeader>
+			<CardContent>
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+						<div className='space-y-4'>
+							<FormField
+								control={form.control}
+								name='name'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Name</FormLabel>
+										<FormControl>
+											<Input
+												{...field}
+												placeholder='John Doe'
+												disabled={isPending}
+											/>
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+						</div>
+						<FormError message={error} />
+						<FormSuccess message={success} />
+						<Button disabled={isPending} type='submit'>
+							Save
+						</Button>
+					</form>
+				</Form>
+			</CardContent>
+		</Card>
 	)
 }
 
--- auth.ts
+++ auth.ts
@@ -6,6 +6,7 @@ import authConfig from '@/auth.config'
 import { getUserById } from '@/data/user'
 import { UserRole } from '@/lib/generated/prisma'
 import { getTwoFactorConfirmationByUserId } from '@/data/two-factor-conformation'
+import { getAccountByUserId } from './data/account'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	pages: {
@@ -58,6 +59,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 			}
 
 			if (session.user) {
+				session.user.name = token.name as string
+				session.user.email = token.email as string
+				session.user.isOAuth = token.isOAuth as boolean
 				session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean
 			}
 
@@ -70,7 +74,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 
 			if (!existingUser) return token
 
+			const existingAccount = await getAccountByUserId(existingUser.id)
+
+			token.name = existingUser.name
+			token.email = existingUser.email
 			token.role = existingUser.role
+			token.isOAuth = !!existingAccount
 			token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled
 
 			return token
