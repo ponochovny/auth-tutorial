eb3db4013176164685ac7bef0d46a43105d8c38c
ponochovny <ponochovny@gmail.com>
2025-08-16 23:41:17 +0300

    refactor: Layout height

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const ProtectedLayout = ({ children }: Props) => {
 	return (
-		<div className='h-full w-full flex flex-col gap-y-10 items-center justify-center bg-radial-[at_50%_75%] from-sky-400 to-blue-800'>
+		<div className='min-h-full w-full justify-center items-center flex flex-col gap-y-10 bg-radial-[at_50%_75%] from-sky-400 to-blue-800'>
 			<Navbar />
 			{children}
 		</div>
